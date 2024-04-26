a8d2ed054e4827567efb4acdefd32f4f32cde791
jabbarlee <amiljabbarlee@gmail.com>
2024-04-26 16:54:29 -0500

    hover design changed

---
+++ app/components/PromptField.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PromptField = () => {
+  return (
+    <div>
+      <div>
+        <button>+</button>
+        <input/>
+        <button>Send</button>
+      </div>
+    </div>
+  )
+}
+
+export default PromptField
