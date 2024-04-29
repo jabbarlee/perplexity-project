e865d29271ae93653218439aad92fca6a6679a78
jabbarlee <amiljabbarlee@gmail.com>
2024-04-29 18:08:23 -0500

    prompt field created

--- app/components/PromptField.tsx
+++ app/components/PromptField.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import React from 'react'
 
 const PromptField = () => {
+
   return (
     <div className='new-prompt-field-wrapper'>
-      <div className='new-prompt'>
-        <button className='attach-button'>+</button>
-        <input type='text' placeholder='Ask a follow-up'/>
-        <button className='send-button'>Send</button>
+      <div className='new-prompt-wrapper'>
+        <div className='new-prompt'>
+            <button className='attach-button'>+</button>
+            <input placeholder='Ask a follow-up' className='prompt-input'/>
+            <button className='send-button'>Send</button>
+        </div>
       </div>
     </div>
   )
